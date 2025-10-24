1fe328c9b2d494babf761309411b00e8090b8382
dutabrahmana <dutalinggau25@gmail.com>
2025-10-24 10:08:59 +0700

    menambahkan komponen Text untuk menampilkan nama prodi dan universitas dari string resources

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -37,3 +37,14 @@ fun ActivitasPertama(modifier: Modifier){
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Text(
+            text = stringResource( id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource( id = R.string.univ),
+            fontSize = 20.sp
+
+        )
+        }
